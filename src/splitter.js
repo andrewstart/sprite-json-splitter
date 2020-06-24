215fc5fcea0d9d6bd87b8bce663dfe0eb78ffaf4
dmytro.shykalenko <dmytro.shykalenko@p2h.com>
2020-06-24 18:25:42 +0300

    take in account original images offset to keep it consistent to prev version

--- src/splitter.js
+++ src/splitter.js
@@ -4,7 +4,13 @@ const path = require('path');
 exports.execute = async function splitter(image, destination, quality, frames) {
     const sprite = await jimp.read(image);
 
-    for (let [filename, {frame: {x, y, w, h}}] of Object.entries(frames)) {
-        sprite.clone().crop(x, y, w, h).quality(quality).write(path.join(destination, filename));
+    for (let [filename, {frame: {x, y, w, h}, spriteSourceSize: {x: sX, y: sY}}] of Object.entries(frames)) {
+        const newImage = sprite.clone().crop(x, y, w, h).quality(quality);
+
+        if (sX || sY) {
+            newImage.contain(sX + w, sY + h, jimp.HORIZONTAL_ALIGN_RIGHT | jimp.VERTICAL_ALIGN_BOTTOM);
+        }
+
+        newImage.write(path.join(destination, filename));
     }
 };
